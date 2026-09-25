f2c56ecf284a36427df5e49e0b7713247d05f591
c0np4nn4 <c0np4nn4@gmail.com>
2026-09-25 22:48:40 +0900

    fin2

--- scripts/check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-cd "$script_dir/.."
-cargo fmt --all -- --check
-cargo clippy --locked --workspace --all-targets -- -D warnings
-cargo test --locked --workspace --release
